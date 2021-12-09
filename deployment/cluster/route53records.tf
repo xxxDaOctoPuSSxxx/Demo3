864e622c26e57bfc14d677d2aa94fcfdf79b5619
Roman Hryshchenko <bookoowest@gmail.com>
2021-12-10 01:48:26 +0200

    try change domain record

--- deployment/cluster/route53records.tf
+++ deployment/cluster/route53records.tf
@@ -12,7 +12,7 @@
 
 resource "aws_route53_record" "bot" {
   zone_id = data.aws_route53_zone.jbot.zone_id
-  name    = "${var.sub_domain_name}"
+  name    = "${var.domain_name}"
   type    = "A"
   alias {
     name = aws_alb.main.dns_name
